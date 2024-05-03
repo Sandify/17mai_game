30e171114668b195638cae5b9b86e980b5635b71
Sandify <101810166+Sandify@users.noreply.github.com>
2024-05-03 13:26:13 +0200

    inventory new v0.1

--- world.gd
+++ world.gd
@@ -14,13 +14,13 @@ func _process(delta):
 	#if Input.is_action_just_pressed("inventory"):
 		#inventory_popup.show()
 
-func _input(event):
-	if event is InputEventKey and event.pressed:
-		if event.keycode == KEY_TAB:
-			if not popup_open:
-				inventory_popup.show()
-				popup_open = true
-			else:
-				inventory_popup.hide()
-				popup_open = false
-	return true
+#func _input(event):
+	#if event is InputEventKey and event.pressed:
+		#if event.keycode == KEY_TAB:
+			#if not popup_open:
+				#inventory_popup.show()
+				#popup_open = true
+			#else:
+				#inventory_popup.hide()
+				#popup_open = false
+	#return true
