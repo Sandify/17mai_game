68b6b917d845df2874103976280616c16f90f391
Sandify <sandergithub@gmail.com>
2024-05-12 16:13:36 +0200

    Update Food_inv.gd

--- Scenes/Food_inv.gd
+++ Scenes/Food_inv.gd
@@ -1,16 +1,22 @@
 extends Label
 
 var food = 10
+@onready var food_timer = $"../Food_timer"
+signal out_of_food
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	self.text = "Food: " + str(food)
-
+	
+	if food < 1:
+		print("out of food")
+		emit_signal("out_of_food")
+		food_timer.stop()
 
 func _on_tree_tree_cut_down():
 	var random_generator = RandomNumberGenerator.new()
@@ -19,3 +25,7 @@ func _on_tree_tree_cut_down():
 	
 	if random_number == 1:
 		food += 2
+
+
+func _on_food_timer_timeout():
+	food -= 1
